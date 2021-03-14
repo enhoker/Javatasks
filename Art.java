72f19ec70e9fc5dbeb31f6f794425acce00a0197
enhoker <45732134+enhoker@users.noreply.github.com>
2021-03-14 23:10:04 +0300

    Add files via upload

---
+++ Art.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Art {
+    String name;
+    boolean ended;
+    int size;
+    String arter;
+
+    public void putData(String name, String arter, int size, boolean ended) {
+        this.arter = arter;
+        this.name = name;
+        this.ended = ended;
+        this.size = size;
+    }
+}
+
